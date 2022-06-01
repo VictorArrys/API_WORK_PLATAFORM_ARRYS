70364850176058a3589e8547bc919b3d6101e5f2
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-06-01 18:35:32 -0500

    Se agrega certificado ssl para la conexion a la base de datos de Mysql

--- utils/keys.js
+++ utils/keys.js
@@ -1,9 +1,14 @@
+const fs = require("fs");
+
 module.exports = {
     database: {
         host : 'elcamelloapi.mysql.database.azure.com',
         database : 'deser_el_camello',
         user : 'elCamello_',
         password: "red_1729.42_",
-        port: 3306
+        port: 3306,
+        ssl: {
+            ca : fs.readFileSync(__dirname + '/certificates/deser_el_camello_db.pem')
+        }
     }
 }
