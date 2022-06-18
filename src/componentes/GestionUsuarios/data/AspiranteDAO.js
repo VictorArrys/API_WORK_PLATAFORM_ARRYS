e216bd521059fcbeb05aa645070cdfdcfdc801e3
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2022-06-18 16:14:19 -0500

    Gestion UsuarioActualizado

--- src/componentes/GestionUsuarios/data/AspiranteDAO.js
+++ src/componentes/GestionUsuarios/data/AspiranteDAO.js
@@ -4,31 +4,38 @@ const {UsuarioDAO} = require('./UsuarioDAO')
 
 var mysqlConnection = require('../../../../utils/conexion');
 
+var mensajes = require('../../../../utils/mensajes');
+
 exports.AspiranteDAO = class AspiranteDAO {
     static getAspirantes(callback) {
         var query = 'SELECT * FROM perfil_aspirante;';
         mysqlConnection.query(query, (error, resultadoConsulta) => {
             if(error) {
-                callback(error);
+                callback(500, mensajes.errorInterno);
             } else {
                 var listaAspirantes = [];
-                resultadoConsulta.forEach(elemento => {
-                    var aspirante = new Aspirante();
-                    aspirante.idPerfilAspirante = elemento['id_perfil_aspirante'];
-                    //Faltan datos
-                    OficioDAO.getOficios(aspirante.idPerfilAspirante, (errorOficios, resultadoOficios)=> {
-                        if(errorOficios) {
-                            callback(errorOficios);
-                        } else {
-                            //resultadoOficios es un arreglo de <<Oficio>>
-                            aspirante.oficios = resultadoOficios;
-                            listaAspirantes.push(aspirante);
-                            if (listaAspirantes.length == resultadoConsulta.length) {
-                                callback(null, listaAspirantes);
+                if (resultadoConsulta.length == 0) {
+                    callback(200, []);
+                } else {
+                    resultadoConsulta.forEach(elemento => {
+                        var aspirante = new Aspirante();
+                        aspirante.idPerfilAspirante = elemento['id_perfil_aspirante'];
+                        //Faltan datos
+                        OficioDAO.getOficios(aspirante.idPerfilAspirante, (errorOficios, resultadoOficios)=> {
+                            if(errorOficios) {
+                                callback(errorOficios);
+                            } else {
+                                //resultadoOficios es un arreglo de <<Oficio>>
+                                aspirante.oficios = resultadoOficios;
+                                listaAspirantes.push(aspirante);
+                                if (listaAspirantes.length == resultadoConsulta.length) {
+                                    callback(200, listaAspirantes);
+                                }
                             }
-                        }
+                        });
                     });
-                });
+                }
+
             }
         });
     }
